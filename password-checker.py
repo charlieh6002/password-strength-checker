ed9f061c72176f198de9625cd497970987f540cf
Charlie Haddock <charliehaddock@Charlies-MacBook-Pro.local>
2025-12-30 22:29:04 +0000

    defined a strong password. created check method

--- password-checker.py
+++ password-checker.py
@@ -3,15 +3,50 @@
 class Checker():
     
     def __init__(self):
-        pass
+        self.special_characters = ["!","@","£","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]",":",";","","'","|",",","<",".",">","?","/","±","€","~","`"]
 
     def check(self):
-        pass
+        length = False
+        upper = False
+        lower = False
+        number = False
+        special = False
+        invalid_character = ""
+
+        if len(user_password) >= 8:
+            length = True
+
+        for c in user_password:
+            if c.isupper():
+                upper = True
+            elif c.islower():
+                lower = True
+            elif c.isnumeric():
+                number = True
+            #use set of special characters
+            else:
+                if c in self.special_characters:
+                    special = True
+                else:
+                    invalid_character = c
+        if invalid_character != "":
+            return ("you have entered an invalid character, " + invalid_character)
+        
+        elif length and upper and lower and number and special:
+            return ("strong password")
+
+        else:
+            return ("weak password")
 
 #main loop
 
 run = True
 checker = Checker()
+print("passwords should: ")
+print("be at least 8 characters in length")
+print("contain at least 1 uppercase and lowercase character")
+print("contain at least 1 number")
+print("contain at least 1 special character")
 
 while run:
     user_password = input("enter password: ")
