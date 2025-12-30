b367341aa24103bb9b12d1b7e7ef8058b753658d
Charlie Haddock <charliehaddock@Charlies-MacBook-Pro.local>
2025-12-30 22:13:35 +0000

    added main loop to repeatedly check inputted passwords

--- password-checker.py
+++ password-checker.py
@@ -1,4 +1,22 @@
 #password-checker.py
 
 class Checker():
-    pass
+    
+    def __init__(self):
+        pass
+
+    def check(self):
+        pass
+
+#main loop
+
+run = True
+checker = Checker()
+
+while run:
+    user_password = input("enter password: ")
+    print(checker.check(user_password))
+    user_input = input("continue? y/n?")
+    if user_input.lower() == "n":
+        print("quitting...")
+        run = False
