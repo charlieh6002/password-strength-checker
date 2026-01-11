4663aa781a6730b539ef5abf08723f2e3818313d
Charlie Haddock <charliehaddock@Charlies-MacBook-Pro.local>
2026-01-11 17:06:59 +0000

    fixed sizing issue with entry box

--- password-checker.py
+++ password-checker.py
@@ -84,9 +84,9 @@ Label(window, text = "contain at least 1 number", font = small_pixeboy_font, bac
 Label(window, text = "contain at least 1 special character", font = small_pixeboy_font, background = "black").place(relx = 0.05, rely = 0.4, anchor = 'nw')
 
 #text entry
-
+password_font = font.Font(family = "Arial", size = 30)
 Label(window, text = "Password: ", font = small_pixeboy_font, background = "black").place(relx = 0.05, rely = 0.5, anchor = "nw")
-password_entry = Entry(window, show = "*", fg = "black", bg = "white", width = 100, text = "Password:").place(relx = 0.2, rely = 0.55, anchor = "nw")
+password_entry = Entry(window, show = "*", fg = "black", bg = "white", width = 40, text = "Password:", font = password_font).place(relx = 0.2, rely = 0.5, anchor = "nw")
 
 window.mainloop()
 #----MAIN-LOOP----
